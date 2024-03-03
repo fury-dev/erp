335896734eee0591d1a01a049a37110ee5210366
fury-r <rajeev.dessai11@gmail.com>
2024-03-03 09:44:10 +0530

    update testcases imports

--- backend/src/utils/__tests__/hashPassword.test.ts
+++ backend/src/utils/__tests__/hashPassword.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from "@jest/globals";
-import { checkIfHasMatches, encryptPassword } from "../hashPassword";
+import { checkIfHasMatches, hashPassword } from "../hashPassword";
 describe("hashPassword", () => {
   it("hash Password and match hash", async () => {
     const test = "test";
-    const hashed = await encryptPassword(test);
+    const hashed = await hashPassword(test);
 
     expect(test).not.toEqual(hashed);
     const matchPassword = await checkIfHasMatches(hashed, test);
